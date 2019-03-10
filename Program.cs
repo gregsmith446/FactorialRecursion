a8cfc2726c754439565c52325c0c7f0aa025e512
Greg Smith <gregsmith446@gmail.com>
2019-03-09 23:23:15 -0500

    recursion with factorials

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactorialRecursion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // to test the recursive factorial function below, change the int in the () + write to console
+            int result = factorial(3);
+        }
+
+
+        static int factorial(int n)
+        {
+            if (n < 1)
+            {
+                return 1;
+            } else
+            {
+                int total = n * factorial(n - 1);
+                return total;
+            }
+        }
+
+    }
+}
